ccf0b5b003ee5dd75166f95ec396302017de2174
Shaikh Sameer <samsk7774@gmail.com>
2024-07-13 14:39:57 +0600

    express server setup

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log('smasher ');
+import app from './app'
+
+// configuring server to run on specific port 
+const startServer = () => {
+    const port = process.env.PORT || 3000
+    app.listen(port, () => {
+        console.log(`Server is running  on  http://localhost:${port}`);
+    })
+}
+
+startServer()
