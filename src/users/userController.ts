044603e16df40c7e15c8aa29944acd1612dc922e
Shaikh Sameer <samsk7774@gmail.com>
2024-07-15 13:23:08 +0600

    add login endpoint

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -7,7 +7,7 @@ import { sign } from 'jsonwebtoken'
 import { config } from '../config/config'
 
 
-// User controller 
+// Create user controller 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // Data validation 
     const { name, email, password } = req.body
@@ -15,21 +15,52 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         const error = createHttpError(400, "All fields are required")
         next(error)
     }
-    // checking user is available or not int the database 
-    const user = await userModal.findOne({ email })
-    if (user) {
-        const error = createHttpError(400, "User Already exist with this username ")
+    // checking user is available or not int the database
+    try {
+        const user = await userModal.findOne({ email })
+        if (user) {
+            const error = createHttpError(400, "User Already exist with this username ")
+        }
+        // hashing password 
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await userModal.create({
+            name, email, password: hashedPassword
+        })
+        // JWT token generation 
+        const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: '7d' });
+        res.status(201).json({ accessToken: token });
+    } catch (error) {
+        next(createHttpError(500, 'error while Creating user  '))
     }
-    // hashing password 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await userModal.create({
-        name, email, password: hashedPassword
-    })
-    // JWT token generation 
-    const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: '7d' });
-    
-
-    res.json({ accessToken: token });
 }
 
-export { createUser }
+// User login controller 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+
+    const { email, password } = req.body
+    if (!email || !password) {
+        return next(createHttpError(400, "All fields are required"))
+    }
+    try {
+        const user = await userModal.findOne({ email });
+        if (!user) {
+            return next(createHttpError(404, "User  not found "))
+        }
+        console.log('user');
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        console.log('isMatch', isMatch);
+
+        if (!isMatch) {
+            return next(createHttpError(400, "Invalid username or password "))
+        }
+        const token = sign({ sub: user._id }, config.jwtSecret as string, { expiresIn: '7d' });
+        res.status(200).json({ accessTokenss: token })
+
+    } catch (error) {
+        next(createHttpError(500, 'Error While login '))
+    }
+
+
+}
+export { createUser, loginUser }
--- src/users/userRouter.ts
+++ src/users/userRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { deflate } from "zlib";
-import { createUser } from "./userController";
+import { createUser, loginUser } from "./userController";
 
 const userRouter = express.Router()
 
 
 userRouter.post('/register', createUser)
+userRouter.post('/login', loginUser)
 
 
 export default userRouter
