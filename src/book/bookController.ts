597e80cf990c3d4b2ee3891393a3f71e7cfc8076
Shaikh Sameer <samsk7774@gmail.com>
2024-07-16 15:40:26 +0600

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -5,11 +5,13 @@ import { Multer } from 'multer'
 import path from 'node:path'
 import { config } from '../config/config'
 import createHttpError from 'http-errors'
-
+import bookModal from './bookModal'
+import fs from 'node:fs'
 
 // Create user controller 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const { title, genre } = req.body
         const files = req.files as { [fielname: string]: Express.Multer.File[]; }
         const coverImageMimetype = files.coverImage[0].mimetype.split('/').at(-1) // splitting by / nad taking last indexx 
         const fileName = files.coverImage[0].filename;
@@ -20,21 +22,28 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
             format: coverImageMimetype
         })
         const bookPath = path.resolve(__dirname, '../../data/uploads', files.file[0].filename,)
-
-        const BookUploadResult = await cloudinary.uploader.upload(filePath, {
+        const bookUploadResult = await cloudinary.uploader.upload(filePath, {
             resource_type: 'raw',
             filename_override: files.file[0].filename,
             folder: 'Books',
             format: 'PDF'
         })
+        // Saving book data to database 
+        const NewBook = await bookModal.create({
+            title: title,
+            genre: genre,
+            author: "6694c19c86a2d9cfaac4fba2",
+            coverImage: ImageUploadResult.secure_url,
+            file: bookUploadResult.secure_url
+        });
+        // deleting temp files 
+        await fs.promises.unlink(filePath)
+        await fs.promises.unlink(bookPath)
 
-        console.log(BookUploadResult);
 
-
-        res.json({ ok: BookUploadResult })
+        res.status(201).json({ id: NewBook._id })
     } catch (error) {
         next(createHttpError(500, 'Internal server error '))
-
     }
 }
 
